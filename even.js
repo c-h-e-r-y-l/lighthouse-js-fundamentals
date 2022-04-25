f57b24e74a0b5047da329398984cc79938cf474b
Cheryl S <103986353+c-h-e-r-y-l@users.noreply.github.com>
2022-04-25 22:42:39 +0000

    Use a function to check if number is even

---
+++ even.js
@@ -0,0 +1,10 @@
+const isEven = function(num) {
+  return num%2 === 0;
+}
+
+//const tenIsEven = isEven(10);
+//const elevenIsEven = isEven(11);
+//commented above var declartion to shorten code and run in console.log directly
+
+console.log(isEven(10));
+console.log(isEven(11));
