6e65ee4b5bcb6b8b4e308ea2a8184b9c51d3d18f
Cheryl S <103986353+c-h-e-r-y-l@users.noreply.github.com>
2022-04-27 23:14:57 +0000

    Added while loop to iterate over array

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,16 @@
 const packingList = ['bowls', 'plates', 'pots', 'pans', 'eating utensils', 'glasses', 'cups', 'cooking utensils'];
 
 console.log(packingList);
+
+//for loop to iterate over array packingList
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+//while loop to iterate over array packingList
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
